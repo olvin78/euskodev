9ccda4cfb77fb47422dd8b0e671918dc1426f382
Olvin Duarte <olvind78@gmail.com>
2025-03-21 13:16:44 +0100

    correo

--- applications/home/views.py
+++ applications/home/views.py
@@ -35,7 +35,7 @@ def formulario_contactar(request):
         print("POST con estos datos:",name,email,phone,company,message,recaptcha_response)
 
         # Enviar correo con los datos ingresados desde la web
-        asunto = f"Tu Asistente Inteligente Nuevo mensaje de contacto de {name}"
+        asunto = f"Euskodev Nuevo mensaje de contacto de {name}"
         contenido = (
             f"Nombre: {name}\n"
             f"Email: {email}\n"
@@ -49,11 +49,12 @@ def formulario_contactar(request):
             send_mail(
                 asunto,
                 contenido,
-                settings.EMAIL_HOST_USER,  # Remitente
-                ["euskodev@gmail.com"],  # Cambia por el correo real
+                settings.EMAIL_HOST_USER,
+                ["info@euskodev.eus"],
                 fail_silently=False,
             )
             print("Enviado email")
+
             messages.success(request, "Tu mensaje ha sido enviado con éxito.")
             return redirect("home_app:home")
 
--- euskodev/settings.py
+++ euskodev/settings.py
@@ -181,8 +181,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "euskodev@gmail.com"
-EMAIL_HOST_PASSWORD = 'nqkg bxps wtjf xwcc '  # Usa la contraseña generada
+EMAIL_HOST_USER = 'info@euskodev.eus'  # <- debe coincidir 100% con la cuenta de Google
+EMAIL_HOST_PASSWORD = 'pjgf buph daxf drvg'# Usa la contraseña generada, azrt cnop xfxh scbq 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 DEFAULT_CHARSET = 'utf-8'
 EMAIL_USE_LOCALTIME = True  # Asegura que Django maneje bien la codificación
@@ -194,3 +194,4 @@ RECAPTCHA_PRIVATE_KEY = "6LfjFPEqAAAAABBc48ipUn3encwA_Ix350EJgQE1"
 
 # Opcional: Para desactivar en desarrollo
 RECAPTCHA_VERIFY_REQUESTS = False  # False si no quieres validar en local
+RECAPTCHA_USE_SSL = True
